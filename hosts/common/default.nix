127871adb020dbdb6245c5fde010e39c0545c1fa
dromelien3 <dromelien3@gatech.edu>
2026-05-25 00:01:04 -0400

    consolidating unnecessary abstractions

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -1,8 +1,31 @@
 { config, pkgs, lib, ... }:
 {
-  imports = [
-    ../../modules/base.nix
-  ];
+  nixpkgs.config.allowUnfree = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "yes";
+      PasswordAuthentication = true;
+    };
+  };
 
   boot.loader.grub.enable = false;
+
+  services.netbird.enable = true;
+
+  users.users.root.initialPassword = "nix";
+
+  security.sudo.wheelNeedsPassword = false;
+
+  environment.systemPackages = with pkgs; [
+    helix
+    curl
+    wget
+    git
+  ];
+
+  system.stateVersion = "25.11";
 }
