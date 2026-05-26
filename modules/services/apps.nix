d580a361d7204eeaa3e70d1bd46a9cec4131052c
dromelien3 <dromelien3@gatech.edu>
2026-05-26 12:48:21 -0400

    forgot a semi colon

--- modules/services/apps.nix
+++ modules/services/apps.nix
@@ -62,7 +62,7 @@ in
     config = {
       adminpassFile = "/etc/nextcloud-admin-pass";
       dbtype = "pgsql";
-    }
+    };
 
     extraApps = {
       inherit (config.services.nextcloud.package.packages.apps) news contacts calendar tasks;
