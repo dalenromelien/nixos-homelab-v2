c0f23bd5033362352342a86b83cafc02a32d8c83
dromelien3 <dromelien3@gatech.edu>
2026-08-08 16:00:52 -0400

    commenting out additional settings for adguard

--- modules/services/apps.nix
+++ modules/services/apps.nix
@@ -18,38 +18,37 @@ in
     host = "127.0.0.1";
     port = ports.adguard;
     mutableSettings = true;
-    settings = {
-      dhcp = {
-        enabled = true;
-        interface_name = [ "eno1" ];
-        dhcpv4 = {
-          gateway_ip = "192.168.1.1";
-          range_start = "192.168.1.101";
-          range_end = "192.168.1.200";
-          lease_duration = "24h";
-        };
-        local_domain_name = "home";
-        dns = [ "192.168.1.100" "1.1.1.1" ];
-      };
+    # settings = {
+    #   dhcp = {
+    #     enabled = true;
+    #     interface_name = [ "eno1" ];
+    #     dhcpv4 = {
+    #       gateway_ip = "192.168.1.1";
+    #       range_start = "192.168.1.101";
+    #       range_end = "192.168.1.200";
+    #       lease_duration = "24h";
+    #     };
+    #     local_domain_name = "home";
+    #   };
 
-      dns = {
-        bind_hosts = ["0.0.0.0"];
-        upstream_dns = [
-          "9.9.9.9#dns.quad9.net"
-          "149.112.112.112#dns.quad9.net"
-          "1.1.1.1"
-        ];
-      };
+    #   dns = {
+    #     bind_hosts = ["0.0.0.0"];
+    #     upstream_dns = [
+    #       "9.9.9.9#dns.quad9.net"
+    #       "149.112.112.112#dns.quad9.net"
+    #       "1.1.1.1"
+    #     ];
+    #   };
 
-      filtering = {
-        protection_enabled = true;
-        filtering_enabled = true;
-        parental_enabled = false;
-        safe_search.enabled = false;
-      };
+    #   filtering = {
+    #     protection_enabled = true;
+    #     filtering_enabled = true;
+    #     parental_enabled = false;
+    #     safe_search.enabled = false;
+    #   };
 
-      filters = map (url: { enabled = true; url = url; }) adguardFilterUrls;
-    };
+    #   filters = map (url: { enabled = true; url = url; }) adguardFilterUrls;
+    # };
   };
 
   # Nextcloud database (required for multi-service reliability)
