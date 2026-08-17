73ed40c1a0c4145acbcf95d2093e2ca63637e4d2
dromelien3 <dromelien3@gatech.edu>
2026-08-17 15:28:14 -0400

    trying to bind nginx to 8080

--- modules/services/networking.nix
+++ modules/services/networking.nix
@@ -7,9 +7,6 @@ in
 {
   services.caddy.enable = true;
 
-  # need to disable nginx to let caddy run
-  services.nginx.enable = false;
-
   networking.hostName = "nixos-homelab";
   networking.useDHCP = lib.mkDefault true;
   networking.firewall.allowedTCPPorts = [22 53 80 443];
@@ -30,6 +27,15 @@ in
       reverse_proxy http://127.0.0.1:${toString ports.nextcloud}
       tls internal
     '';
+  };
+
+  # This is needed so NGINX doesn't bind to 80/443 and nextcloud can run.
+  services.nginx = {
+    enable = true;
+    virtualHosts."nextcloud.home" = {
+      listen = [{ addr = "127.0.0.1"; port = 8080; }];
+      # module injects the rest into this same vhost block
+    };
   };
 
   services.adguardhome.settings.filtering.rewrites = [
