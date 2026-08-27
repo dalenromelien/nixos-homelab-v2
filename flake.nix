9d2883b4d12f1e53d771088a0748ecc1ed479ada
dromelien3 <dromelien3@gatech.edu>
2026-08-27 12:47:20 -0400

    trying unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     disko = {
       url = "github:nix-community/disko/latest";
